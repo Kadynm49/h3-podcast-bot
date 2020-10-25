e59f9d4dee370932d6ddcfcbcb1487f41d762552
Kadyn Marshall <kadynm49@github.com>
2020-10-25 21:39:19 +0000

    heyo

--- add_punctuation.py
+++ add_punctuation.py
@@ -1,9 +1,9 @@
 from punctuator import Punctuator
-p = Punctuator('/home/kadyn/h3/.punctuator/Demo-Europarl-EN.pcl')
+p = Punctuator('/home/erviewre/h3podcastbot/.punctuator/Demo-Europarl-EN.pcl')
 
 import os
 
-directory = os.fsencode("/home/kadyn/h3/raw_scripts")
+directory = os.fsencode("/home/erviewre/h3podcastbot/raw_scripts")
 file_count = len(os.listdir(directory))
 progress = 1
 
@@ -11,7 +11,7 @@ for file in os.listdir(directory):
   print(str(progress) + "/" + str(file_count))
   progress += 1
   filename = os.fsdecode(file)
-  with open('/home/kadyn/h3/raw_scripts/' + filename, 'r') as open_file:
+  with open('/home/erviewre/h3podcastbot/raw_scripts/' + filename, 'r') as open_file:
     data = open_file.read().replace('\n', ' ')
-    with open('/home/kadyn/h3/punctuated_scripts/' + filename, 'w') as punctuated_file:
+    with open('/home/erviewre/h3podcastbot/punctuated_scripts/' + filename, 'w') as punctuated_file:
       punctuated_file.write(p.punctuate(data))
